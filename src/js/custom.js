c4147ac49a212ecb35d046fc4d8b3f097adcc68e
Anastasiya Datsyuk <anastasiya.datsyuk@uvoteam.com>
2017-03-27 19:10:22 +0300

    new page

--- src/js/custom.js
+++ src/js/custom.js
@@ -1,2 +1,17 @@
+$('body').imagesLoaded()
+    .always( function( instance ) {
+        console.log('all images loaded');
+    })
+    .done( function( instance ) {
+        console.log('all images successfully loaded');
+    })
+    .fail( function() {
+        console.log('all images loaded, at least one is broken');
+    })
+    .progress( function( instance, image ) {
+        var result = image.isLoaded ? 'loaded' : 'broken';
+        console.log( 'image is ' + result + ' for ' + image.img.src );
+    });
 $(document).ready(function() {
+
 })
